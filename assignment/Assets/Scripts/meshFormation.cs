45a6578bebe58e1b02f27e94e2018acb72c8161d
Nicolas Condrea <c18424946@mytudublin.ie>
2021-12-01 16:57:01 +0000

    Creating vertices and adding triangles to the mesh

---
+++ assignment/Assets/Scripts/meshFormation.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meshFormation : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] trianglePoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreatePoints();
+
+        // Applying changes to mesh
+        UpdateMesh();
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    private void CreatePoints()
+    {
+
+        // Adding the vertices points in an array
+        vertices = new Vector3[]
+        {
+            new Vector3 (0, 0, 0),
+            new Vector3 (0, 0, 1),
+            new Vector3 (1, 0, 0)
+        };
+
+        // Adding triangles
+        trianglePoints = new int[]
+        {
+            // points must be in clockwise order or else backface culling will occur
+            0, 1, 2
+        };
+    }
+
+    private void UpdateMesh()
+    {
+        // Clearing any posible vertices and triangle data
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = trianglePoints;
+    }
+}
